cf2fd2a62a76f47d04a4e6a5a9584f1a8a36970e
snova <awaawapurin@gmail.com>
2020-05-21 13:58:06 +0900

    updated

--- diary/admin.py
+++ diary/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Diary
+from .models import Diary, Category
 
 admin.site.register(Diary)
+admin.site.register(Category)
--- diary/models.py
+++ diary/models.py
@@ -2,8 +2,20 @@ from django.db import models
 from django.utils import timezone
 
 
+class CreatedAt(models.Model):
+    name = models.CharField('作成日', max_length=255)
+
+
+class Category(models.Model):
+    name = models.CharField('カテゴリ名', max_length=255)
+    created_at = models.ForeignKey(CreatedAt, verbose_name='作成日', null=True, blank=True, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
+
 class Diary(models.Model):
-    category = models.CharField('カテゴリ', max_length=255)
+    category = models.ForeignKey(Category, verbose_name='カテゴリ', on_delete=models.PROTECT)
     text = models.TextField('本文')
     created_at = models.DateTimeField('作成日', default=timezone.now)
 
