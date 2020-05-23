50fd265e7c38eb8f063f6d1b30b0813bfb9792e4
snova <awaawapurin@gmail.com>
2020-05-23 12:47:12 +0900

    updated

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'reviews'
 
 urlpatterns = [
-    path('', views.review_list, name='review_list'),
-    path('detail/<int:pk>/', views.review_detail, name='review_detail'),
-    path('create/', views.review_create, name='review_create'),
-    path('update/<int:pk>/', views.review_update, name='review_update'),
-    path('delete/<int:pk>/', views.review_delete, name='review_delete'),
+    path('', views.ReviewList.as_view(), name='review_list'),
+    path('detail/<int:pk>/', views.ReviewDetail.as_view(), name='review_detail'),
+    path('create/', views.ReviewCreate.as_view(), name='review_create'),
+    path('update/<int:pk>/', views.ReviewUpdate.as_view(), name='review_update'),
+    path('delete/<int:pk>/', views.ReviewDelete.as_view(), name='review_delete'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,51 +1,30 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic
+from django.urls import reverse_lazy
 from .forms import ReviewCreateForm
 from .models import Review
 
 
-def review_list(request):
-    context = {
-        'review_list': Review.objects.all().order_by('-created_at'),
-    }
-    return render(request, 'reviews/review_list.html', context)
+class ReviewList(generic.ListView):
+    model = Review
 
 
-def review_detail(request, pk):
-    context = {
-        'review': get_object_or_404(Review, pk=pk),
-    }
-    return render(request, 'reviews/review_detail.html', context)
+class ReviewDetail(generic.DetailView):
+    model = Review
 
 
-def review_create(request):
-    form = ReviewCreateForm(request.POST or None)
-    if request.method == 'POST' and form.is_valid():
-        form.save()
-        return redirect('reviews:review_list')
-    context = {
-        'form': form
-    }
-    return render(request, 'reviews/review_form.html', context)
+class ReviewCreate(generic.CreateView):
+    model = Review
+    form_class = ReviewCreateForm
+    success_url = reverse_lazy('reviews:review_list')
 
 
-def review_update(request, pk):
-    review = get_object_or_404(Review, pk=pk)
-    form = ReviewCreateForm(request.POST or None, instance=review)
-    if request.method == 'POST' and form.is_valid():
-        form.save()
-        return redirect('reviews:review_list')
-    context = {
-        'form': form
-    }
-    return render(request, 'reviews/review_form.html', context)
+class ReviewUpdate(generic.UpdateView):
+    model = Review
+    form_class = ReviewCreateForm
+    success_url = reverse_lazy('reviews:review_list')
 
 
-def review_delete(request, pk):
-    review = get_object_or_404(Review, pk=pk)
-    if request.method == 'POST':
-        review.delete()
-        return redirect('reviews:review_list')
-    context = {
-        'review': review
-    }
-    return render(request, 'reviews/review_confirm_delete.html', context)
+class ReviewDelete(generic.DeleteView):
+    model = Review
+    success_url = reverse_lazy('reviews:review_list')
